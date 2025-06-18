c1331dc43dfa4d1017fc7f67cafb22ed4eb1dff4
zoeyChenCap <capchen113@163.com>
2025-06-18 13:40:20 -0500

    task for p1 branch completed

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # Define what the app does
+# @app.route("/greet", methods=["GET"]))
 @app.get("/greet")
 def index():
     """
@@ -14,4 +15,31 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
-    return jsonify("TODO")
+
+    # if name(data) is not passed as a query string, capture it and use it programatically
+    # 1. Capture first name and last name
+    fname = request.args.get("fname")
+    lname = request.args.get("lname")
+
+    # 2. If either is not provided: respond with an error
+    if not fname and not lname:
+        return ({"status": "error"})
+    
+    # 3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
+    elif not fname and lname:
+        response = {"data": f"Hello Mr {lname}!"}
+
+    # 4. If first name is provided but second name is not provided: respond with "Hello, <first-name>!"
+    elif fname and not lname:
+        response = {"data": f"Hello, {fname}!"}
+    
+    # 5. If both names are provided: respond with a question, "Is your name <first-name> <second-name>?"
+    else:
+        response = {"data": f"Is your name {fname} {lname}?"}
+        
+    # name = request.args.get("name")
+    # if not name:
+    #     return ({"status": "error"})
+    
+    # response = {"data": f"Hello, {name}!"}
+    return jsonify(response)
